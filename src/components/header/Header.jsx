20cad1dac5f2368307d5c652710a2942ee976024
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-07-20 22:02:12 +0530

    nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import { BiMenu } from 'react-icons/bi'
 
 import logo from '../../assets/images/logo.png'
 import avatar from '../../assets/images/avatar-icon.png'
+import { useEffect, useRef } from 'react'
 
 const navLinks = [
   {
@@ -24,8 +25,32 @@ const navLinks = [
 ] 
 
 const Header = () => {
+
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', () => {
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header')
+      }else {
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() => {
+    handleStickyHeader()
+
+    return () => window.removeEventListener('scroll', handleStickyHeader)
+  }, [])
+
+  const toggleMenu = () => {
+    menuRef.current.classList.toggle('show__menu')
+  }
+
   return (
-    <header className="header">
+    <header className="header flex items-center justify-center" ref={headerRef}>
       <div className="container">
         <nav className="flex items-center justify-between">
           {/* Logo */}
@@ -34,7 +59,7 @@ const Header = () => {
           </div>
 
           {/* Menu */}
-          <div className='navigation'>
+          <div className='navigation' ref={menuRef} onClick={toggleMenu}>
             <ul className='menu flex items-center gap-[2.7rem]'>
               {
                 navLinks.map((item, index) => {
@@ -50,7 +75,7 @@ const Header = () => {
           
           {/* nav right */}
           <div className='flex items-center gap-4'>
-              <div>
+              <div className='hidden'>
                 <Link to='/'></Link>
                 <figure className='w-[35px] rounded-full'>
                   <img className='w-full rounded-full' src={avatar} alt="userAvatar" />
@@ -61,7 +86,7 @@ const Header = () => {
                 <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[55px]'>Login</button>
               </Link>
 
-              <span className='md:hidden'>
+              <span className='md:hidden' onClick={toggleMenu}>
                 <BiMenu className='w-6 h-6 cursor-pointer' />
               </span>
 
